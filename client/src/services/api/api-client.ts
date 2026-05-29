7836f616da715ee85c817274b9e17315ad8e2bba
rohithbairi9 <rohithbairi9@gmail.com>
2026-05-29 11:20:08 +0530

    Configure production API URL

--- client/src/services/api/api-client.ts
+++ client/src/services/api/api-client.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:5000/api",
   withCredentials: true,
 });
